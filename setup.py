dab55533979febc628750550e1923f734939820e
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-07-08 13:53:00 -0500

    minor version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='juno-magic',
-      version='0.1.8',
+      version='0.1.9',
       description='IPython magics and utilities to work with bridged kernels',
       url='https://github.com/timbr-io/juno-magic',
       author='Pramukta Kumar',
