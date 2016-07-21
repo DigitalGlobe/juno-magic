de3ede1c085f8437e9df5f0d2e411f29237e211d
Chris Helm <christopher.helm@gmail.com>
2016-07-21 10:16:40 -0600

    updating the setup develop and install with a cmdclass to enable the nbextension by default

--- setup.py
+++ setup.py
@@ -2,21 +2,26 @@ from setuptools import setup
 from setuptools.command.develop import develop as _develop
 from notebook.nbextensions import install_nbextension
 from notebook.services.config import ConfigManager
-import os.path
+import os
 
-juno_extension_dir = os.path.join(os.path.dirname(__file__), "juno_magic")
-
+juno_extension_dir = os.path.join(os.path.dirname(__file__), "juno_magic", "static")
 
 class develop(_develop):
     def run(self):
         _develop.run(self)
         install_nbextension(juno_extension_dir, symlink=True,
-                            overwrite=True, user=False, prefix="/Users/prak/anaconda2/envs/juno2")
+                            overwrite=True, user=False, destination="juno_magic")
         cm = ConfigManager()
-        cm.update('notebook', {"load_extensions": {"juno_magic/static/extension": True } })
+        cm.update('notebook', {"load_extensions": {"juno_magic/extension": True } })
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        cm = ConfigManager()
+        cm.update('notebook', {"load_extensions": {"juno_magic/extension": True } })
 
 setup(name='juno-magic',
-      cmdclass={'develop': develop},
+      cmdclass={'develop': develop, 'install': install},
       version='0.1.9',
       description='IPython magics and utilities to work with bridged kernels',
       url='https://github.com/timbr-io/juno-magic',
