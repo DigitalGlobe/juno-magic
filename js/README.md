7bf5e5c0017de056677d4580efd32010205011fd
Chris Helm <christopher.helm@gmail.com>
2016-11-17 14:55:01 -0700

    new setup.py nbextension install

---
+++ js/README.md
@@ -0,0 +1,19 @@
+# Developing javascript juno-magic 
+
+# Install
+
+Note: Juno-magic currently relies on the `@timbr/react-machine` repo for its UI. In order to develop the JS locally you need to have the `timbr-omni` package locally, and then run 'npm link' in the `packages/react-machine` repo. 
+
+1. Run `npm install`
+2. python setup.py develop
+3. pip install .
+
+# Usage 
+
+After the installation is complete, you should be able to start a notebook and see the extension appear. 
+
+```
+jupyter notebook
+```
+
+
--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 from setuptools import setup
+from setuptools.command.develop import develop as _develop
+from setuptools.command.install import install as _install
+from notebook.nbextensions import install_nbextension
+from notebook.services.config import ConfigManager
+import os
+
+extension_dir = os.path.join(os.path.dirname(__file__), "juno_magic", "static")
+
+class develop(_develop):
+    def run(self):
+        _develop.run(self)
+        install_nbextension(extension_dir, symlink=True,
+                            overwrite=True, user=True, destination="juno_magic")
+        cm = ConfigManager()
+        cm.update('notebook', {"load_extensions": {"juno_magic/index": True } })
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        cm = ConfigManager()
+        cm.update('notebook', {"load_extensions": {"juno_magic/index": True } })
 
 setup(name='juno-magic',
-      version='0.1.11',
+      cmdclass={'develop': develop, 'install': install},
+      version='0.1.12',
       description='IPython magics and utilities to work with bridged kernels',
       url='https://github.com/timbr-io/juno-magic',
       author='Pramukta Kumar',
