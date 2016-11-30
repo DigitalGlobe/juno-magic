9d7fddf62bfa335f97f8697c94ffc3c3278611d3
glortho <jed@dataverse.me>
2016-11-30 16:17:11 -0500

    optimized webpack config

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require( 'webpack' );
 const envVars = [ 'NODE_ENV' ];
 
 const babelSettings = {
+  cacheDirectory: true,
   plugins: [
     'add-module-exports',
     'transform-regenerator',
@@ -13,6 +14,7 @@ const babelSettings = {
 };
 
 module.exports = [{
+  cache: true,
   entry: './src/index.js',
   output: {
     filename: 'index.js',
@@ -20,33 +22,16 @@ module.exports = [{
     libraryTarget: 'amd'
   },
   module: {
-    //preLoaders: [{
-      //test: /\.js$/,
-      //loader: 'source-map-loader'
-    //}],
     loaders : [{
-      test: /\.gif$/,
-      loader: 'url-loader'
-    }, {
-      test: /\.png$/,
-      loader: 'url-loader'
-    }, {
       test: /\.js$/,
       exclude: /(node_modules|bower_components)/,
       loaders: [`babel?${JSON.stringify( babelSettings )}`]
     }, {
       test: /\.css$/,
       loader: 'style-loader!css-loader?modules&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]&importLoaders=1!postcss-loader?sourceMap'
-    }, {
-      test: /\.(woff|woff2|eot|ttf|svg)$/,
-      loader: 'url-loader?name=fonts/[name].build-[hash].[ext]&limit=10000'
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader'
     }]
   },
   resolve: { 
-    fallback: path.join( __dirname, "node_modules" ),
     alias: {
       react: path.resolve('./node_modules/react'),
       'react-dom': path.resolve('./node_modules/react-dom')
@@ -57,20 +42,9 @@ module.exports = [{
     net: "empty",
     tls: "empty"
   },
-  //devtool: 'inline-source-map',
   plugins: [
     new webpack.EnvironmentPlugin( envVars ),
-    new webpack.NoErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.IgnorePlugin(/^mock-firmata$/),
     new webpack.ContextReplacementPlugin(/bindings$/, /^$/)
-  ],
-  postcss: function() {
-    return [
-      require( 'postcss-import' )({ addDependencyTo: webpack }),
-      require( 'postcss-modules-values' ),
-      require( 'postcss-nested' ),
-      require( 'autoprefixer' )
-    ];
-  }
+  ]
 }];
