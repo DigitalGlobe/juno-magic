462f471dfcf0997a7ea31eec20becfa83adf4315
Jamie Polackwich <rjpolackwich@gmail.com>
2017-05-31 02:44:44 -0400

    adding docstring inheritance to custom autobahn-dervired twisted error classes in lieu of writing our own since they'd probably be incorrect. TODO raise an issue/ make PR w/ autobahn about it since it'd be useful to have those errors anyway

--- juno_magic/exception.py
+++ juno_magic/exception.py
@@ -1,20 +1,33 @@
 from twisted.internet.error import ConnectError, ConnectionLost
+import inspect
+
+# From StackOverflow: https://stackoverflow.com/questions/13937500/inherit-a-parent-class-docstring-as-doc-attribute
+def inheritdocstring(name, bases, attrs):
+    if not '__doc__' in attrs:
+        # create a temporary 'parent' to (greatly) simplify the MRO search
+        temp = type('temporaryclass', bases, {})
+        for cls in inspect.getmro(temp):
+            if cls.__doc__ is not None:
+                attrs['__doc__'] = cls.__doc__
+                break
+
+    return type(name, bases, attrs)
 
 class CloseHandshakeError(ConnectionLost):
-    pass
+    __metaclass__ = inheritdocstring
 
 class MaxFramePayloadSizeExceededError(ConnectionLost):
-    pass
+    __metaclass__ = inheritdocstring
 
 class MaxMessagePayloadSizeExceededError(ConnectionLost):
-    pass
+    __metaclass__ = inheritdocstring
 
 class OpenHandshakeTimeoutError(ConnectError):
-    pass
+    __metaclass__ = inheritdocstring
 
 class ServerConnectionDropTimeoutError(ConnectionLost):
-    pass
+    __metaclass__ = inheritdocstring
 
 class ServingFlashSocketPolicyFileError(ConnectionLost):
-    pass
+    __metaclass__ = inheritdocstring
 
