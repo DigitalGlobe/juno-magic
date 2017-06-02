19cc6107b54b4b24291044623bca6df44a234aa0
Jamie Polackwich <rjpolackwich@gmail.com>
2017-06-02 10:32:41 -0400

    copying and pasting from twisted exception docstrings

--- juno_magic/exception.py
+++ juno_magic/exception.py
@@ -1,33 +1,20 @@
 from twisted.internet.error import ConnectError, ConnectionLost
-import inspect
-
-# From StackOverflow: https://stackoverflow.com/questions/13937500/inherit-a-parent-class-docstring-as-doc-attribute
-def inheritdocstring(name, bases, attrs):
-    if not '__doc__' in attrs:
-        # create a temporary 'parent' to (greatly) simplify the MRO search
-        temp = type('temporaryclass', bases, {})
-        for cls in inspect.getmro(temp):
-            if cls.__doc__ is not None:
-                attrs['__doc__'] = cls.__doc__
-                break
-
-    return type(name, bases, attrs)
 
 class CloseHandshakeError(ConnectionLost):
-    __metaclass__ = inheritdocstring
+    """Connection to the other side was lost in a non-clean fashion"""
 
 class MaxFramePayloadSizeExceededError(ConnectionLost):
-    __metaclass__ = inheritdocstring
+    """Connection to the other side was lost in a non-clean fashion"""
 
 class MaxMessagePayloadSizeExceededError(ConnectionLost):
-    __metaclass__ = inheritdocstring
+    """Connection to the other side was lost in a non-clean fashion"""
 
 class OpenHandshakeTimeoutError(ConnectError):
-    __metaclass__ = inheritdocstring
+    """An error occurred while connecting"""
 
 class ServerConnectionDropTimeoutError(ConnectionLost):
-    __metaclass__ = inheritdocstring
+    """Connection to the other side was lost in a non-clean fashion"""
 
 class ServingFlashSocketPolicyFileError(ConnectionLost):
-    __metaclass__ = inheritdocstring
+    """Connection to the other side was lost in a non-clean fashion"""
 
